989d6417367080717daa7c25790e414d47136079
gdavidss <davidguilherme.david@gmail.com>
2022-10-07 10:48:49 -0300

    Auto saved by Logseq

--- _plugins/markdown-highlighter.rb
+++ _plugins/markdown-highlighter.rb
@@ -9,7 +9,7 @@ end
 Jekyll::Hooks.register [:pages], :post_convert do |doc|
   # jekyll considers anything at the root as a page,
   # we only want to consider actual pages
-  next unless doc.path.start_with?('pages/')
+  next unless doc.path.start_with?('_pages/')
   replace(doc)
 end
 
