c210e374fb560d38b5fe49f14c3adeca2f9de31a
gdavidss <davidguilherme.david@gmail.com>
2022-10-13 16:38:17 -0300

    Auto saved by Logseq

--- _plugins/bidirectional_links_generator.rb
+++ _plugins/bidirectional_links_generator.rb
@@ -78,6 +78,7 @@ class BidirectionalLinksGenerator < Jekyll::Generator
       # Nodes: Jekyll
       all.notes.each do |e|
       begin notes_linking_to_current_note = all_notes.filter do |e|
+        binding.pry
         e.content.include?(current_note.url)
       end
 
